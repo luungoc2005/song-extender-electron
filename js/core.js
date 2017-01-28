c3640284df3c1c5031a3936af19bd3200fc55f2b
DESKTOP-8CL99LH\Gasto <luungoc2005@gmail.com>
2017-01-28 14:45:26 +0700

    Revert "attempt at using the worker only for processing (not working)"
    This reverts commit 48c294f33ef1ba2355c3f8b5b82e6841178153e5.

--- js/core.js
+++ js/core.js
@@ -13,18 +13,6 @@ const url = require('url')
 let cp;
 let mainWindow;
 
-// audio file ops
-const Speaker = require('speaker');
-const AudioDownsampler = require('./stream/audioDownsampler.js');
-const StereoToMonoReader = require('./stream/stereoToMonoReader.js');
-const FFTWriter = require('./stream/fftWriter.js');
-const FFTAnalyser = require('./analyser.js');
-
-let transform;
-let downsample;
-let fft;
-let fftCount;
-
 function sendToMainWindow(event, data) 
 {
     if (!mainWindow) mainWindow = BrowserWindow.getFocusedWindow();
@@ -51,88 +39,30 @@ ipc.on('open-file-dialog', function (event)
         var baseName = encodeURIComponent(path.basename(uri));
         var dirName = path.dirname(uri);
         event.sender.send('selected-file', path.join(dirName, baseName));
-        // console.log(`Sending message - ${cp.send({ fileName : uri })}`);
-        openFile(uri);
+        //openFile(uri);
+        if (!cp) 
+        {
+            var workerPath = path.join(__dirname, 'worker.js');
+            cp = ChildProcess.fork(workerPath)
+            
+            cp.on('message', function (data)
+            {
+                //console.log(JSON.stringify(data));
+                sendToMainWindow(data.event, data.path);
+            });
+
+            cp.on('close', function (code, signal) 
+            {
+                console.log(`child process terminated with code ${code} due to receipt of signal ${signal}`);
+            });
+
+            cp.on('error', function(error)
+            {
+                console.log(`child process encountered error ${error}`);
+            });
+        }
+        console.log(`Sending message - ${cp.send({ fileName : uri })}`);
+        // cp.send({ fileName : uri })
     }
   })
 })
-
-function addChildHandlers()
-{
-    if (!cp)
-    {        
-        // cp = ChildProcess.fork(workerPath)
-        cp.on('close', function (code, signal) 
-        {
-            console.log(`child process terminated with code ${code} due to receipt of signal ${signal}`);
-        });
-
-        cp.on('error', function(error)
-        {
-            console.log(`child process encountered error ${error}`);
-        });
-        
-        cp.on('message', function (data)
-        {
-            sendToMainWindow(data.event, data.path);
-        });
-    }
-}
-
-function openFile(file)
-{
-    var buffer = Buffer.alloc(65536);
-    var stream = fs.createReadStream(file);
-    var reader;
-    switch (path.extname(file).toLowerCase())
-    {
-        case '.mp3':
-            var lame = require('lame');
-            reader = new lame.Decoder();
-            break;
-        case '.wav':
-            var wav = require('wav');
-            reader = new wav.Reader();
-            break;
-        default:
-            break;
-    }
-    reader.on('format', function (format)
-    {
-        transform = new StereoToMonoReader(format);
-        downsample = new AudioDownsampler(format);
-        speaker = new Speaker(format);
-        fft = new FFTWriter(format, 2048);
-        fftCount = 0;
-        var fftBuffer = [];
-        var workerPath = path.join(__dirname, 'worker.js');
-
-        fft.on('finish', function()
-        {
-            console.log("Finish reading file");
-            onHandledError(`Analysing ...`);
-            
-            var childMessage = function (_path, _data) { return { path: _path, data: _data }; }
-
-            cp = ChildProcess.fork(workerPath);
-            addChildHandlers();            
-            cp.send(childMessage('format', format));
-            for (var i = 0; i < fftBuffer.length; i++)
-            {
-                cp.send(childMessage('fft', fftBuffer[i]));
-            }
-            cp.send(childMessage('calc', null));
-        });
-
-        fft.on('fft', function (fftResult)
-        {
-            onHandledError(`Preprocessing ...`);
-            fftBuffer.push(fftResult);
-        });
-        
-        onHandledError(`File opened: ${file}`);
-        reader.pipe(transform).pipe(downsample).pipe(fft);
-    });
-
-    stream.pipe(reader);
-}
